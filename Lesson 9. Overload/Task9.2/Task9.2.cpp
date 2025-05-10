667187575a7fd00b798185275fdee36ad193a58d
Mikhail <aabbcc4q@gmail.com>
2025-05-10 20:32:17 +0300

    Add Lesson 9

---
+++ Lesson 9. Overload/Task9.2/Task9.2.cpp
@@ -0,0 +1,147 @@
+﻿// Task9.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	unsigned int greatest_common_divisor(unsigned int a, unsigned int b) {
+		if (a % b == 0)
+			return b;
+		if (b % a == 0)
+			return a;
+		if (a > b)
+			return greatest_common_divisor(a % b, b);
+		return greatest_common_divisor(a, b % a);
+	}
+	void simplify() {
+		int gcd = greatest_common_divisor(std::abs(numerator_), std::abs(denominator_));
+		numerator_ /= gcd;
+		denominator_ /= gcd;
+		if (denominator_ < 0) {
+			denominator_ *= -1;
+			numerator_ *= -1;
+		}
+	}
+	bool operator==(Fraction fract_right) {
+		return ((((double)numerator_ / denominator_) == ((double)fract_right.numerator_ / fract_right.denominator_)) ? true : false);
+	}
+	bool operator!=(Fraction fract_right) {
+		return (!(*this == fract_right));
+	}
+	bool operator<(Fraction fract_right) {
+		return ((((double)numerator_ / denominator_) < ((double)fract_right.numerator_ / fract_right.denominator_)) ? true : false);
+	}
+	bool operator>(Fraction fract_right) {
+		return (fract_right < *this);
+	}
+	bool operator<=(Fraction fract_right) {
+		return !(*this > fract_right);
+	}
+	bool operator>=(Fraction fract_right) {
+		return !(*this < fract_right);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Fraction& fraction);
+
+	Fraction operator+(const Fraction& fract_right) {
+		Fraction fraction(this->numerator_ * fract_right.denominator_ + fract_right.numerator_ * this->denominator_,
+			this->denominator_ * fract_right.denominator_);
+		fraction.simplify();
+		return fraction;
+	}
+	Fraction operator-(const Fraction& fract_right) {
+		Fraction fraction(this->numerator_ * fract_right.denominator_ - fract_right.numerator_ * this->denominator_,
+			this->denominator_ * fract_right.denominator_);
+		fraction.simplify();
+		return fraction;
+	}
+	Fraction operator*(const Fraction& fract_right) {
+		Fraction fraction(this->numerator_ * fract_right.numerator_,
+			this->denominator_ * fract_right.denominator_);
+		fraction.simplify();
+		return fraction;
+	}
+	Fraction operator/(const Fraction& fract_right) {
+		Fraction fraction(this->numerator_ * fract_right.denominator_,
+			this->denominator_ * fract_right.numerator_);
+		fraction.simplify();
+		return fraction;
+	}
+	Fraction& operator++() {
+		this->numerator_ += this->denominator_;
+		return *this;
+	}
+	Fraction& operator--() {
+		this->numerator_ -= this->denominator_;
+		return *this;
+	}
+	Fraction operator++(int) {
+		Fraction temp = *this;
+		++(*this);
+		return temp;
+	}
+	Fraction operator--(int) {
+		Fraction temp = *this;
+		--(*this);
+		return temp;
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const Fraction& fraction) {
+	os << fraction.numerator_ << "/" << fraction.denominator_;
+	return os;
+}
+
+int main()
+{
+	int numerator1, denominator1, numerator2, denominator2;
+
+	std::setlocale(LC_ALL, "Russian");
+
+	std::cout << "Введите числитель дроби 1: ";
+	std::cin >> numerator1;
+	std::cout << "Введите знаменатель дроби 1: ";
+	std::cin >> denominator1;
+	std::cout << "Введите числитель дроби 2: ";
+	std::cin >> numerator2;
+	std::cout << "Введите знаменатель дроби 2: ";
+	std::cin >> denominator2;
+
+	Fraction f1(numerator1, denominator1);
+	Fraction f2(numerator2, denominator2);
+
+	std::cout << f1 << " + " << f2 << " = " << f1 + f2 << std::endl;
+	std::cout << f1 << " - " << f2 << " = " << f1 - f2 << std::endl;
+	std::cout << f1 << " * " << f2 << " = " << f1 * f2 << std::endl;
+	std::cout << f1 << " / " << f2 << " = " << f1 / f2 << std::endl;
+	std::cout << "++" << f1 << " * " << f2 << " = ";
+	std::cout << (++f1) * f2 << std::endl;
+	std::cout << "Значение дроби 1 = " << f1 << std::endl;
+	std::cout << f1 << "--" << " * " << f2 << " = ";
+	std::cout << (f1--) * f2 << std::endl;
+	std::cout << "Значение дроби 1 = " << f1 << std::endl;
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
